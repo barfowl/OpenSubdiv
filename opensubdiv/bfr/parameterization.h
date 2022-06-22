b2c6d6b4159dd79f745518a9952672aaab55aaf2
Barry Fowler <barfowl@gmail.com>
2022-06-22 16:59:44 -0700

    Moved implementation details of Parameterization to source file:     - created parameterization.cpp from former header-only implementation     - added explicit instantiation for necessary template methods     - reduced public header dependencies in parameterization.h     - fixed implicit header dependency in tessellation.cpp (<cassert>)

--- opensubdiv/bfr/parameterization.h
+++ opensubdiv/bfr/parameterization.h
@@ -27,11 +27,7 @@
 
 #include "../version.h"
 
-#include "../bfr/limits.h"
 #include "../sdc/types.h"
-
-#include <cmath>
-#include <cassert>
 
 namespace OpenSubdiv {
 namespace OPENSUBDIV_VERSION {
@@ -40,8 +36,9 @@ namespace Bfr {
 
 //
 //  Parameterization is a simple class that provides information about the
-//  parameterization of faces of a particular size for a given subdivision
-//  scheme.
+//  parameterization of a face in a local (u,v) coordinate system. That
+//  information is determined given the size of a face (i.e. its number of
+//  vertices) and the subdivision scheme used to subdivide it.
 //
 //  The subdivision scheme is essential in determining how a face is
 //  parameterized, e.g. a triangle is regular for the Loop scheme and so
@@ -51,9 +48,10 @@ namespace Bfr {
 class Parameterization {
 public:
     //
-    //  The three kinds of parameterizations:  quadrilateral, triangle and
-    //  quadrangulated N-sided polygon.  This is not intended for general
-    //  use, but is made public for clients that do need to distinguish:
+    //  The three kinds of parameterizations defined are:  quadrilateral,
+    //  triangle and quadrangulated sub-faces.  This is not intended for
+    //  common use, but is publicly available for situations when it is
+    //  necessary to distinguish:
     //
     enum Type { QUAD, TRI, QUAD_SUBFACES };
 
@@ -89,7 +87,7 @@ public:
     //  Methods to deal with discontinuous parameterizations, i.e. those
     //  partitioned into sub-faces:
     //
-    bool HasSubFaces() const { return (_type == QUAD_SUBFACES); }
+    bool HasSubFaces() const;
 
     template <typename REAL>
     int GetSubFace(REAL const uvCoord[2]) const;
@@ -116,11 +114,11 @@ public:
                 REAL const subFaceCoord[2], REAL uvCoord[2]) const;
 
 private:
-    template <typename REAL, bool NORMALIZED>
-    int convertCoordToSubFace(
+    template <typename REAL>
+    int convertCoordToSubFace(bool normalized,
                 REAL const uvCoord[2], REAL subFaceCoord[2]) const;
-    template <typename REAL, bool NORMALIZED>
-    void convertSubFaceToCoord(int subFace,
+    template <typename REAL>
+    void convertSubFaceToCoord(bool normalized, int subFace,
                 REAL const subFaceCoord[2], REAL uvCoord[2]) const;
 
 private:
@@ -130,161 +128,17 @@ private:
 };
 
 //
-//  Inline construction and resizing methods:
+//  Inline sub-face coordinate conversion methods:
 //
-inline
-Parameterization::Parameterization(Sdc::SchemeType scheme, int faceSize) {
-
-    int regFaceSize = Sdc::SchemeTypeTraits::GetRegularFaceSize(scheme);
-
-    _type     = (unsigned char) ((regFaceSize == 4) ? QUAD : TRI);
-    _faceSize = (unsigned short) std::min(faceSize, Limits::MaxFaceSize());
-    _uDim     = 0;
-
-    if (_faceSize != regFaceSize) {
-        if (_faceSize < 3) {
-            //  Reset size to 0 (invalid) for degenerate faces of all schemes:
-            _faceSize = 0;
-        } else if (regFaceSize == 3) {
-            //  Reset size to 0 (invalid) for non-triangles of tri schemes:
-            _faceSize = 0;
-        } else {
-            //  Quad sub-faces -- use int sqrt for udim to preserve accuracy:
-            _type = QUAD_SUBFACES;
-            _uDim = (_faceSize < 10) ?
-                    (unsigned char)(2 + (_faceSize > 4)) :
-                    (unsigned char)(1 + (int) std::sqrt((float)(_faceSize-1)));
-        }
-    }
-}
-
-//
-//  Simple coordinate queries:
-//
-template <typename REAL>
-void
-Parameterization::GetVertexCoord(int vertex, REAL uv[2]) const {
-
-    switch (GetType()) {
-    case QUAD:
-        uv[0] = (REAL) (vertex && (vertex < 3));
-        uv[1] = (REAL) (vertex > 1);
-        break;
-    case TRI:
-        uv[0] = (REAL) (vertex == 1);
-        uv[1] = (REAL) (vertex == 2);
-        break;
-    case QUAD_SUBFACES:
-        uv[0] = (REAL) (vertex % _uDim);
-        uv[1] = (REAL) (vertex / _uDim);
-        break;
-    default:
-        uv[0] = -1.0f;
-        uv[1] = -1.0f;
-        break;
-    }
+inline bool
+Parameterization::HasSubFaces() const {
+    return (_type == QUAD_SUBFACES);
 }
 
 template <typename REAL>
-void
-Parameterization::GetEdgeCoord(int edge, REAL t, REAL uv[2]) const {
-
-    switch (GetType()) {
-    case QUAD:
-        switch (edge) {
-        case 0: uv[0] = t;        uv[1] = 0.0f;     break;
-        case 1: uv[0] = 1.0f;     uv[1] = t;        break;
-        case 2: uv[0] = 1.0f - t; uv[1] = 1.0f;     break;
-        case 3: uv[0] = 0.0f;     uv[1] = 1.0f - t; break;
-        }
-        break;
-
-    case TRI:
-        switch (edge) {
-        case 0: uv[0] = t;        uv[1] = 0.0f;     break;
-        case 1: uv[0] = 1.0f - t; uv[1] = t;        break;
-        case 2: uv[0] = 0.0f;     uv[1] = 1.0f - t; break;
-        }
-        break;
-
-    case QUAD_SUBFACES:
-        if (t < 0.5f) {
-            GetVertexCoord(edge, uv);
-            uv[0] += t;
-        } else {
-            GetVertexCoord((edge + 1) % _faceSize, uv);
-            uv[1] += 1.0f - t;
-        }
-        break;
-    default:
-        uv[0] = -1.0f;
-        uv[1] = -1.0f;
-        break;
-    }
-}
-
-template <typename REAL>
-void
-Parameterization::GetCenterCoord(REAL uv[2]) const {
-
-    if (GetType() == TRI) {
-        uv[0] = 1.0f / 3.0f;
-        uv[1] = 1.0f / 3.0f;
-    } else {
-        uv[0] = 0.5f;
-        uv[1] = 0.5f;
-    }
-}
-
-//
-//  Sub-face coordinate conversion methods:
-//
-template <typename REAL>
-int
-Parameterization::GetSubFace(REAL const uvCoord[2]) const {
-
-    return HasSubFaces() ? (_uDim * (int)uvCoord[1] + (int)uvCoord[0]) : 0;
-}
-
-//  Private conversions used by the public conversions:
-template <typename REAL, bool NORMALIZED>
 inline int
-Parameterization::convertCoordToSubFace(
-        REAL const uvCoord[2], REAL subCoord[2]) const {
-
-    assert(HasSubFaces());
-
-    //  Be sure this assignment always supports conversion in-place:
-    int uTile = (int) uvCoord[0];
-    int vTile = (int) uvCoord[1];
-
-    if (NORMALIZED) {
-        subCoord[0] = (uvCoord[0] - (REAL) uTile) * 2.0f;
-        subCoord[1] = (uvCoord[1] - (REAL) vTile) * 2.0f;
-    } else {
-        subCoord[0] = (uvCoord[0] - (REAL) uTile);
-        subCoord[1] = (uvCoord[1] - (REAL) vTile);
-    }
-    return _uDim * vTile + uTile;
-}
-template <typename REAL, bool NORMALIZED>
-inline void
-Parameterization::convertSubFaceToCoord(
-        int subFace, REAL const subCoord[2], REAL uvCoord[2]) const {
-
-    assert(HasSubFaces());
-
-    //  Be sure this assignment always supports conversion in-place:
-    int uTile = subFace % _uDim;
-    int vTile = subFace / _uDim;
-
-    if (NORMALIZED) {
-        uvCoord[0] = (REAL) uTile + subCoord[0] * 0.5f;
-        uvCoord[1] = (REAL) vTile + subCoord[1] * 0.5f;
-    } else {
-        uvCoord[0] = (REAL) uTile + subCoord[0];
-        uvCoord[1] = (REAL) vTile + subCoord[1];
-    }
+Parameterization::GetSubFace(REAL const uvCoord[2]) const {
+    return HasSubFaces() ? (_uDim * (int)uvCoord[1] + (int)uvCoord[0]) : 0;
 }
 
 //  Conversions to unnormalized sub-face coordinates:
@@ -292,15 +146,13 @@ template <typename REAL>
 inline int
 Parameterization::ConvertCoordToSubFace(
         REAL const uvCoord[2], REAL subCoord[2]) const {
-
-    return convertCoordToSubFace<REAL,false>(uvCoord, subCoord);
+    return convertCoordToSubFace<REAL>(false, uvCoord, subCoord);
 }
 template <typename REAL>
 inline void
 Parameterization::ConvertSubFaceToCoord(
         int subFace, REAL const subCoord[2], REAL uvCoord[2]) const {
-
-    convertSubFaceToCoord<REAL,false>(subFace, subCoord, uvCoord);
+    convertSubFaceToCoord<REAL>(false, subFace, subCoord, uvCoord);
 }
 
 //  Conversions to normalized sub-face coordinates:
@@ -308,15 +160,13 @@ template <typename REAL>
 inline int
 Parameterization::ConvertCoordToNormalizedSubFace(
         REAL const uvCoord[2], REAL subCoord[2]) const {
-
-    return convertCoordToSubFace<REAL,true>(uvCoord, subCoord);
+    return convertCoordToSubFace<REAL>(true, uvCoord, subCoord);
 }
 template <typename REAL>
 inline void
 Parameterization::ConvertNormalizedSubFaceToCoord(
         int subFace, REAL const subCoord[2], REAL uvCoord[2]) const {
-
-    convertSubFaceToCoord<REAL,true>(subFace, subCoord, uvCoord);
+    convertSubFaceToCoord<REAL>(true, subFace, subCoord, uvCoord);
 }
 
 } // end namespace Bfr
--- opensubdiv/bfr/tessellation.cpp
+++ opensubdiv/bfr/tessellation.cpp
@@ -26,6 +26,7 @@
 
 #include <cstring>
 #include <cstdio>
+#include <cassert>
 #include <limits>
 #include <algorithm>
 
